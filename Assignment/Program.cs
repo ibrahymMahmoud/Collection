4055eed70229dc311724079d6307ea5de943df36
ibrahymMahmoud <ibrahymmahmoud123@gmail.com>
2024-08-06 19:11:40 +0300

    Q01 Given an array of integers, count the frequency of each element using a hash table.

---
+++ Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q01
+            //Given an array of integers, count the frequency of each element using a hash table.
+
+          ///  int[] numbers = { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 1, 2, 3 };
+          ///  Dictionary <int , int> keyValuePairs = new Dictionary<int, int>() { };
+          ///
+          ///  for (int i = 0; i < numbers.Length; i++)
+          ///  {
+          ///      if (keyValuePairs.ContainsKey(numbers[i]))
+          ///      { keyValuePairs[numbers[i]]++; }
+          ///      else
+          ///      { keyValuePairs[numbers[i]] = 1; }
+          ///  }
+          ///
+          ///  foreach (var i in keyValuePairs)
+          ///      Console.WriteLine($"key = {i.Key} is appears {i.Value} times");
+            #endregion
+
+        }
+    }
+}
